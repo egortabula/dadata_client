bb32c674b19a7190bcf9e2a5a5f78a4b893531d4
egortabula <egor.vnl@gmail.com>
2024-10-14 11:39:16 +0300

    feat: add export apis.dart

--- lib/dadata_client.dart
+++ lib/dadata_client.dart
@@ -2,6 +2,7 @@
 /// data validation, and enrichment. Compatible with both Flutter and Dart.
 library;
 
+export 'src/apis/apis.dart';
 export 'src/dadata_client.dart';
 export 'src/enums/enums.dart';
 export 'src/exceptions/exceptions.dart';
